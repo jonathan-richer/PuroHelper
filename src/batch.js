8a0d22f2c14e4d301f376a85d392774d70b4b36c
Jonathan Richer <36356746+jonathan-richer@users.noreply.github.com>
2019-07-30 17:58:11 -0400

    Update batch.js

--- src/batch.js
+++ src/batch.js
@@ -29,16 +29,16 @@ const processBatch = document.getElementById("ctl00_CPPC_btnProcessBatch");
 // « Créer une expédition » et « Traiter l'envoi sélectionné »
 
 createShipment.style["float"]       = "left";
-createShipment.style["background"]  = "#0000FF"
-createShipment.style["color"]       = "#57FF47";
+createShipment.style["background"]  = "#EDA600";
+createShipment.style["color"]       = "#098A00";
 createShipment.style["font-weight"] = "750";
 createShipment.style["font-size"]   = "14px";
 
 
 if (processSingle !== null) {
 	processSingle.style["float"]       = "left";
-	processSingle.style["background"]  = "#57FF47"
-	processSingle.style["color"]       = "#0000FF";
+	processSingle.style["background"]  = "#098A00";
+	processSingle.style["color"]       = "#EDA600";
 	processSingle.style["font-weight"] = "750";
 	processSingle.style["font-size"]   = "14px";
 }
@@ -46,31 +46,93 @@ if (processSingle !== null) {
 
 // « Traiter le lot » confirmation
 if (processBatch !== null) {
-	const btn = document.createElement("button");
+	const btn       = document.createElement("button");
 	const btnCancel = document.createElement("button");
 
-	processBatch.style.display = "none";
+	processBatch.style["display"] = "none";
 
-	btn.innerText = "Traiter le lot";
-	btn.style.display = "inline-block";
-	btn.onclick = e => {
+	btn.innerText           = "Traiter le lot";
+	btn.style["display"]    = "inline-block";
+	btn.style["background"] = "#F0F0EE";
+	btn.style["border"]     = "none";
+
+	btn.addEventListener("click", e => {
 		e.preventDefault();
 	    if (confirm("Êtes-vous certain de vouloir traiter le lot?")) {
-	    	processBatch.style.display = "inline-block";
-			btnCancel.style.display = "inline-block";
-	    	btn.style.display = "none";
+	    	processBatch.style["display"] = "inline-block";
+			btnCancel.style["display"]    = "inline-block";
+	    	btn.style["display"]          = "none";
 	    }
-	}
+	});
 
-	btnCancel.innerText = "Annuler";
-	btnCancel.style.display = "none";
-	btnCancel.onclick = e => {
+	btnCancel.innerText           = "Annuler";
+	btnCancel.style["display"]    = "none";
+	btnCancel.style["background"] = "#000";
+	btnCancel.style["color"]      = "#FFF";
+	btnCancel.style["font-size"]  = "14px";
+	btnCancel.style["border"]     = "none";
+	btnCancel.style["padding"]    = "5px";
+
+	btnCancel.addEventListener("click", e => {
 		e.preventDefault();
-		processBatch.style.display = "none";
-		btnCancel.style.display = "none";
-		btn.style.display = "inline-block";
-	}
+		processBatch.style["display"] = "none";
+		btnCancel.style["display"]    = "none";
+		btn.style["display"]          = "inline-block";
+	});
 
 	processBatch.parentElement.appendChild(btn);
 	processBatch.parentElement.appendChild(btnCancel);
 }
+
+
+/*
+ * Cacher les trucs inutiles.
+ */
+
+const batchName = document.getElementById("ctl00_CPPC_lblBatchContent").innerText;
+const lblTitle  = document.getElementById("ctl00_CPPC_lblTitle");
+console.log(batchName);
+let date = "";
+
+let firstQuote = false;
+for (let i = 0; i < batchName.length; i++) {
+	if (!firstQuote) {
+		if (batchName[i] === "\"") {
+			firstQuote = true;
+		}
+	} else {
+		if (batchName[i] === "\"") {
+			break;
+		} else {
+			date += batchName[i];
+		}
+	}
+}
+lblTitle.innerText += `: "${date}"`;
+
+let hidden     = false;
+const hideBtn  = document.createElement("button");
+const elements = [
+	document.getElementById("ctl00_CPPC_lblInst").parentElement,
+	document.getElementById("ctl00_CPPC_lblInst2").parentElement,
+	document.getElementById("ctl00_CPPC_lblBatchCreated").parentElement.parentElement.parentElement,
+	document.getElementById("ctl00_CPPC_ServiceDateRow")
+];
+
+hideBtn.addEventListener("click", e => {
+	e.preventDefault();
+	hidden = !hidden;
+	for (const e of elements) {
+		e.style["display"] = hidden ? "none" : "block";
+	}
+});
+hideBtn.innerText           = "Afficher / Cacher";
+hideBtn.style["display"]    = "inline-block";
+hideBtn.style["background"] = "purple";
+hideBtn.style["color"]      = "white";
+hideBtn.style["float"]      = "right";
+
+lblTitle.parentElement.style["width"] = "100%";
+lblTitle.parentElement.appendChild(hideBtn);
+
+hideBtn.click();
